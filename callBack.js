e45cc551557f04a160bb98ff80468e53e637003c
Aruna Nuwan <arunanuwan5413044@gmail.com>
2025-01-24 03:53:40 +0530

    Commit one callBack.js

---
+++ callBack.js
@@ -0,0 +1,66 @@
+
+function task1(callback) {
+    setTimeout(() => {
+      console.log(1);
+      callback();
+    }, 1000);
+  }
+  
+  function task2 (callback) {
+    setTimeout(() => {
+      console.log(2);
+     callback();
+    }, 2000);
+  }
+  
+  function task3 (callback) {
+    setTimeout(() => {
+      console.log(3);
+      callback();
+    }, 1500);
+  }
+  
+  function task4 (callback) {
+    setTimeout(() => {
+      console.log(4);
+     callback();
+    }, 3000);
+  }
+  
+  
+  function task5 (callback) {
+    setTimeout(() => {
+      console.log(5);
+      callback();
+    }, 4000);
+  }
+  
+  
+  function task6 (callback) {
+    setTimeout(() => {
+      console.log(6);
+      callback();
+    }, 3500);
+  }
+  
+  
+  function task7 (callback) {
+    setTimeout(() => {
+      console.log(7);
+    }, 500);
+  }
+  
+  task1(() => {
+      task2(() => {
+        task3(() => {
+          task4(() => {
+            task5(() => {
+              task6(() => {
+                task7();
+            })
+          })
+        })
+      })
+    })
+  });
+  
