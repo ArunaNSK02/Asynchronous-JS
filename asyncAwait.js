b82f6d61a154ea76fb5e5e0ca1115dad5eb82f4a
Aruna Nuwan <arunanuwan5413044@gmail.com>
2025-01-29 02:05:07 +0530

    Commit two asyncAwait.js

--- asyncAwait.js
+++ asyncAwait.js
@@ -17,21 +17,3 @@ async function getFile(filePathOne, filePathTwo) {
 }
 
 getFile("../Learn RWD/ResponsiveImageGrid/index.html", "../Learn RWD/ResponsiveImageGrid/style.css");
-
-
-async function getDummyJSON(filePath) {
-    const req = new XMLHttpRequest();
-    req.open('GET', filePath);
-    req.onload = () => {
-      if (req.status == 200) {
-        return req.response;
-      } else {
-        console.log("File unable to serve!");
-      }
-    }
-    req.send();
-}
-
-getDummyJSON('https://dummyjson.com/products/1').then((response) => {
-    console.log(response);
-})
