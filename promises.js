47217d0bf6d1139548a1ef449cd570d0662bbcc9
Aruna Nuwan <arunanuwan5413044@gmail.com>
2025-01-28 23:29:20 +0530

    Commit two promises.js

--- promises.js
+++ promises.js
@@ -1,17 +1,49 @@
-const promise = new Promise((resolve, reject) => {
-    const randomNumber = Math.floor(Math.random() * 10);
+let displayObj = document.getElementById("response-obj");
 
-    setTimeout(() => {
-        if (randomNumber < 4) {
-            resolve("Great!");
-        } else {
-            reject("Oops!");
+function myDisplayer(some) {
+    displayObj.innerHTML = some;
+}
+
+function getFile(filePath) {
+    return new Promise((resolve, reject) => {
+        const req = new XMLHttpRequest();
+        req.open('GET', filePath);
+        req.onload = () => {
+            if (req.status == 200) {
+                resolve(req.response)
+            } else {
+                reject("Error: " + req.status)
+            }
         }
-    }, 3000)
+        req.send();
+    }) 
+}
+
+Promise.all([getFile("../Learn RWD/ResponsiveImageGrid/index.html"), getFile("../Learn RWD/ResponsiveImageGrid/style.css")]).then((values) => {
+    myDisplayer(values[0]);
+    const styleElement = document.createElement("style");
+    styleElement.textContent = values[1];
+    document.head.appendChild(styleElement);
 })
 
-promise.then((result) => {
-    console.log(result);
-}, (reason) => {
-    console.log(reason);
+function getDummyJSON(filePath) {
+    return new Promise((resolve, reject) => {
+        const req = new XMLHttpRequest();
+        req.open('GET', filePath);
+        req.onload = () => {
+            if (req.status == 200) {
+                resolve(req.response)
+            } else {
+                reject("Error: " + req.status)
+            }
+        }
+        req.send();
+    })
+}
+
+getDummyJSON('https://dummyjson.com/products/1').then((response) => {
+    console.log(response);
+})
+.catch((error) => {
+    console.log(error);
 })
